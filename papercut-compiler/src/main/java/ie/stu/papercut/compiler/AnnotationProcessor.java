d6d38287c4b1618ed0272c997efcd1b653e15c05
Stuart Gilbert <stuartg@amazon.com>
2016-11-22 17:32:57 -0800

    Attempting to get the processor working

--- papercut-compiler/src/main/java/ie/stu/papercut/compiler/AnnotationProcessor.java
+++ papercut-compiler/src/main/java/ie/stu/papercut/compiler/AnnotationProcessor.java
@@ -15,6 +15,8 @@
  */
 package ie.stu.papercut.compiler;
 
+import ie.stu.papercut.RemoveThis;
+
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
@@ -22,10 +24,6 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
-import javax.tools.JavaFileObject;
-import java.io.IOException;
-import java.io.Writer;
-import ie.stu.papercut.RemoveThis;
 import java.util.Set;
 
 @SupportedAnnotationTypes("ie.stu.papercut.RemoveThis")
@@ -34,36 +32,16 @@ public class AnnotationProcessor extends AbstractProcessor{
 
     @Override
     public boolean process(final Set<? extends TypeElement> annotations, final RoundEnvironment roundEnv) {
-        final StringBuilder builder = new StringBuilder()
-                .append("package ie.stu.papercut.generated;\n\n")
-                .append("public class GeneratedClass {\n\n") // open class
-                .append("\tpublic String getMessage() {\n") // open method
-                .append("\t\treturn \"");
-
-        // for each javax.lang.model.element.Element annotated with the CustomAnnotation
-        for (Element element : roundEnv.getElementsAnnotatedWith(RemoveThis.class)) {
+        System.out.println("PROCESSING PAPERCUT ANNOTATIONS");
+        for (final Element element : roundEnv.getElementsAnnotatedWith(RemoveThis.class)) {
+            System.out.println("FOUND A REMOVETHIS ANNOTATION");
             final String objectType = element.getSimpleName().toString();
 
-            // this is appending to the return statement
-            builder.append(objectType).append(" says hello!\\n");
-        }
+            if (element.getAnnotation(RemoveThis.class).stopShip()) {
+                System.out.println("ANNOTATION SAYS TO STOP SHIP!");
+                throw new IllegalStateException("STOP SHIP: @RemoveThis specified");
 
-
-        builder.append("\";\n") // end return
-                .append("\t}\n") // close method
-                .append("}\n"); // close class
-
-        try { // write the file
-            final JavaFileObject source
-                    = processingEnv.getFiler().createSourceFile("ie.stu.papercut.generated.GeneratedClass");
-
-            final Writer writer = source.openWriter();
-            writer.write(builder.toString());
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            // Note: calling e.printStackTrace() will print IO errors
-            // that occur from the file already existing after its first run, this is normal
+            }
         }
 
         return true;
