ee02f665c037237994631295fc88c68d86fc78a8
Stuart Gilbert <stuart.gilbert@gmail.com>
2020-11-29 15:18:26 -0800

    Switching to maven-publish and signing plugins for publishing
    After a lot of messing around I finally got the new method to work. This requires you to have some credentials saved in your $GRADLE_USER_HOME/gradle.properties file, named papercutPublishingUsername and papercutPublishingPassword. These credentials should match the ones you enter to login on the Sonatype Nexus website. The username is not an email address.

--- papercut-annotations/build.gradle.kts
+++ papercut-annotations/build.gradle.kts
@@ -1,7 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val papercutPublishingUsername: String by project
+val papercutPublishingPassword: String by project
+
 plugins {
     kotlin("jvm")
+    `java-library`
+    `maven-publish`
+    signing
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
 
 tasks {
@@ -10,4 +21,50 @@ tasks {
     }
 }
 
-apply(from = rootProject.file("gradle/gradle-mvn-push.gradle"))
+publishing {
+    publications {
+        create<MavenPublication>("annotations") {
+            groupId = "ie.stu"
+            artifactId = "papercut-annotations"
+            from(components["java"])
+            pom {
+                name.set("Papercut Annotations")
+                description.set("Keep your codebase simple.")
+                url.set("https://github.com/Stuie/papercut/")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("stuartgilbert")
+                        name.set("Stuart Gilbert")
+                        email.set("stuart.gilbert@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Stuie/papercut.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/Stuie/papercut.git")
+                    url.set("https://github.com/Stuie/papercut/")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            credentials {
+                username = papercutPublishingUsername
+                password = papercutPublishingPassword
+            }
+            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["annotations"])
+}
--- papercut-compiler/build.gradle.kts
+++ papercut-compiler/build.gradle.kts
@@ -1,9 +1,20 @@
 import org.gradle.internal.jvm.Jvm
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val papercutPublishingUsername: String by project
+val papercutPublishingPassword: String by project
+
 plugins {
     kotlin("jvm")
     kotlin("kapt")
+    `java-library`
+    `maven-publish`
+    signing
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
 
 tasks {
@@ -33,4 +44,50 @@ tasks.test {
     }
 }
 
-apply(from = rootProject.file("gradle/gradle-mvn-push.gradle"))
+publishing {
+    publications {
+        create<MavenPublication>("compiler") {
+            groupId = "ie.stu"
+            artifactId = "papercut-compiler"
+            from(components["java"])
+            pom {
+                name.set("Papercut Compiler")
+                description.set("Keep your codebase simple.")
+                url.set("https://github.com/Stuie/papercut/")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("stuartgilbert")
+                        name.set("Stuart Gilbert")
+                        email.set("stuart.gilbert@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Stuie/papercut.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/Stuie/papercut.git")
+                    url.set("https://github.com/Stuie/papercut/")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            credentials {
+                username = papercutPublishingUsername
+                password = papercutPublishingPassword
+            }
+            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["compiler"])
+}
